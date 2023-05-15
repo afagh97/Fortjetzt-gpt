451f2c9da4dec34da46921ada5a7beabc32cfe01
afagh97 <82460018+afagh97@users.noreply.github.com>
2023-05-15 21:11:57 +0200

    Add files via upload

---
+++ Fortjetzt/button.js
@@ -0,0 +1,16 @@
+function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+  
+  window.addEventListener('scroll', function() {
+    var backToTopBtn = document.getElementById('back-to-top-btn');
+    if (window.scrollY > 300) {
+      backToTopBtn.style.display = 'block';
+    } else {
+      backToTopBtn.style.display = 'none';
+    }
+  });
+  
